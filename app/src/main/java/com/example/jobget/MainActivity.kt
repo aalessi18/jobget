c1df6a473fb45052b3d7ab42352d0ece69981003
Alexander Alessi <alexander.alessi@forgedome.com>
2022-05-31 21:46:49 -0400

    Introduced view binding, and added row_data xml file

--- app/src/main/java/com/example/jobget/MainActivity.kt
+++ app/src/main/java/com/example/jobget/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.jobget
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jobget.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var tvExpensesHeading: TextView
+    private lateinit var tvExpensesLabel: TextView
+    private lateinit var tvIncomeHeading: TextView
+    private lateinit var tvIncomeLabel: TextView
+    private lateinit var tvBalanceHeading: TextView
+    private lateinit var tvBalanceLabel: TextView
+    private lateinit var rvTransactions: RecyclerView
+    private lateinit var fabAddButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun initComponents() {
+        binding.apply {
+
+        }
     }
 }
