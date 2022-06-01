4a618536e00b11376069273f607027edc1d70619
Alexander Alessi <alexander.alessi@forgedome.com>
2022-05-31 22:46:59 -0400

    Added TransactionAdapter class

---
+++ app/src/main/java/com/example/jobget/adapter/TransactionAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.jobget.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TransactionAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class DateViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    inner class TransactionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
