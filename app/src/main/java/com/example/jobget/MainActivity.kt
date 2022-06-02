799c0af045217e646e93b94b941d62114b8aa74c
Alexander Alessi <alexander.alessi@forgedome.com>
2022-06-02 15:52:36 -0400

    Renamed layout file and introduced new row xml file for recycleview data

--- app/src/main/java/com/example/jobget/MainActivity.kt
+++ app/src/main/java/com/example/jobget/MainActivity.kt
@@ -24,6 +24,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initComponents()
     }
 
     private fun initComponents() {
