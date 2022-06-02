524057258843ee4844fcbab43621e9a32c55d236
Alexander Alessi <alexander.alessi@forgedome.com>
2022-06-02 16:42:06 -0400

    Added enum to distinguish between transaction types, and added defalt values for data

--- app/src/main/java/com/example/jobget/helper/UserDataHelper.kt
+++ app/src/main/java/com/example/jobget/helper/UserDataHelper.kt
@@ -4,7 +4,11 @@ import android.app.Activity
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import com.example.jobget.model.TransactionModel
+import com.example.jobget.model.TransactionType
 import com.google.gson.Gson
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.Date
 import org.json.JSONArray
 
 object UserDataHelper {
@@ -17,15 +21,27 @@ object UserDataHelper {
         } else {
             userTransactions[date]?.add(transactionModel)
         }
+        addToSharedPreferences(sharedPreferences)
+    }
+
+    fun getTransactions(activity: Activity): JSONArray? {
+        val sharedPreferences: SharedPreferences = activity.getPreferences(MODE_PRIVATE)
+        if (userTransactions.isNullOrEmpty()) {
+            val currentDate = SimpleDateFormat("dd/MMM/yyyy", Locale.getDefault()).format(Date())
+            userTransactions[currentDate] = mutableListOf(
+                TransactionModel("Income", "1000", TransactionType.INCOME),
+                TransactionModel("Shoes", "70", TransactionType.EXPENSE),
+            )
+            addToSharedPreferences(sharedPreferences)
+        }
+        return sharedPreferences.getString("transactions", null) as JSONArray?
+    }
+
+    private fun addToSharedPreferences(sharedPreferences: SharedPreferences) {
         val jsonObject = Gson().toJson(userTransactions)
         with(sharedPreferences.edit()) {
             putString("transactions", jsonObject)
             commit()
         }
-    }
-
-    fun getTransactions(activity: Activity): JSONArray? {
-        val sharedPreferences: SharedPreferences = activity.getPreferences(MODE_PRIVATE)
-        return sharedPreferences.getString("transactions", null) as JSONArray?
     }
 }
--- app/src/main/java/com/example/jobget/model/TransactionModel.kt
+++ app/src/main/java/com/example/jobget/model/TransactionModel.kt
@@ -1,6 +1,9 @@
 package com.example.jobget.model
 
+enum class TransactionType { INCOME, EXPENSE }
+
 data class TransactionModel(
     val transactionTitle: String,
-    val transactionAmount: String
+    val transactionAmount: String,
+    val transactionType: TransactionType
 )
