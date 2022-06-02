d644dbc9464bd35c48b70f7acee6e3353ef79055
Alexander Alessi <alexander.alessi@forgedome.com>
2022-06-02 17:58:42 -0400

    Added application class to allow for hilt dependency injection

--- app/src/main/java/com/example/jobget/MainActivity.kt
+++ app/src/main/java/com/example/jobget/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jobget
 
+import android.app.Application
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
---
+++ app/src/main/java/com/example/jobget/application/JobGetApplication.kt
@@ -0,0 +1,8 @@
+package com.example.jobget.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JobGetApplication : Application() {
+}
