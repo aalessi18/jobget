d2f1a59ff6c929014e3b78da7fdcd0ca0beca68b
Alexander Alessi <alexander.alessi@forgedome.com>
2022-06-03 19:23:46 -0400

    Added spinner default values, and cancel button to close dialog

--- app/src/main/java/com/example/jobget/dialog/AddTransactionDialog.kt
+++ app/src/main/java/com/example/jobget/dialog/AddTransactionDialog.kt
@@ -5,14 +5,19 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.view.Display
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import com.example.jobget.R
+import com.example.jobget.databinding.DialogAddTransactionBinding
 
 
 class AddTransactionDialog(context: Context, private val display: Display) : Dialog(context) {
+    private lateinit var binding: DialogAddTransactionBinding
     private lateinit var activity: Activity
     private lateinit var defaultDisplay: Display
+    private lateinit var spinnerTransactionType: Spinner
     private lateinit var editTextTitle: EditText
     private lateinit var editTextAmount: EditText
     private lateinit var btnAdd: Button
@@ -25,6 +30,18 @@ class AddTransactionDialog(context: Context, private val display: Display) : Dia
     }
 
     private fun initComponents() {
-
+        spinnerTransactionType = findViewById(R.id.sp_transaction_type)
+        ArrayAdapter.createFromResource(
+            context,
+            R.array.transaction_type_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerTransactionType.adapter = adapter
+        }
+        btnCancel = findViewById(R.id.btn_cancel)
+        btnCancel.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
