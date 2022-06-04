c72db160b1b60f1a2c64e91781649b3a9389eab3
Alexander Alessi <alexander.alessi@forgedome.com>
2022-06-04 15:25:00 -0400

    Added functions to retrieve Income, Expense, and Balance

--- app/src/main/java/com/example/jobget/helper/UserDataHelper.kt
+++ app/src/main/java/com/example/jobget/helper/UserDataHelper.kt
@@ -53,7 +53,68 @@ class UserDataHelper @Inject constructor() {
         )
     }
 
-    private fun addToSharedPreferences(sharedPreferences: SharedPreferences, jsonData: MutableMap<String, MutableList<TransactionModel>>) {
+    fun getExpenseTotal(activity: Activity): String {
+        val sharedPreferences: SharedPreferences = activity.getPreferences(MODE_PRIVATE)
+        val json = sharedPreferences.getString("transactions", null)
+        val jsonData: MutableMap<String, MutableList<TransactionModel>> = Gson().fromJson(
+            json,
+            object : TypeToken<MutableMap<String, MutableList<TransactionModel>>>() {}.type
+        )
+
+        var totalExpense = 0;
+        jsonData.forEach { (_, mutableList) ->
+            mutableList.forEach {
+                if (it.transactionType == TransactionType.EXPENSE) {
+                    totalExpense += it.transactionAmount.toInt()
+                }
+            }
+        }
+        return totalExpense.toString()
+    }
+
+    fun getIncomeTotal(activity: Activity): String {
+        val sharedPreferences: SharedPreferences = activity.getPreferences(MODE_PRIVATE)
+        val json = sharedPreferences.getString("transactions", null)
+        val jsonData: MutableMap<String, MutableList<TransactionModel>> = Gson().fromJson(
+            json,
+            object : TypeToken<MutableMap<String, MutableList<TransactionModel>>>() {}.type
+        )
+
+        var totalIncome = 0;
+        jsonData.forEach { (_, mutableList) ->
+            mutableList.forEach {
+                if (it.transactionType == TransactionType.INCOME) {
+                    totalIncome += it.transactionAmount.toInt()
+                }
+            }
+        }
+        return totalIncome.toString()
+    }
+
+    fun getBalanceTotal(activity: Activity): String {
+        val sharedPreferences: SharedPreferences = activity.getPreferences(MODE_PRIVATE)
+        val json = sharedPreferences.getString("transactions", null)
+        val jsonData: MutableMap<String, MutableList<TransactionModel>> = Gson().fromJson(
+            json,
+            object : TypeToken<MutableMap<String, MutableList<TransactionModel>>>() {}.type
+        )
+
+        var totalBalance = 0;
+        jsonData.forEach { (_, mutableList) ->
+            mutableList.forEach {
+                when (it.transactionType == TransactionType.INCOME) {
+                    true -> totalBalance += it.transactionAmount.toInt()
+                    else -> totalBalance -= it.transactionAmount.toInt()
+                }
+            }
+        }
+        return totalBalance.toString()
+    }
+
+    private fun addToSharedPreferences(
+        sharedPreferences: SharedPreferences,
+        jsonData: MutableMap<String, MutableList<TransactionModel>>
+    ) {
         with(sharedPreferences.edit()) {
             putString("transactions", Gson().toJson(jsonData))
             commit()
